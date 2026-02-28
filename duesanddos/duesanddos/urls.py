d85b3336be9fd4e9581d72c09dcbaebc1fdc3a65
Duvuru Yathin Reddy <yathinreddy2004@gmail.com>
2026-02-28 18:55:35 -0500

    CSS for Login Page

--- duesanddos/duesanddos/urls.py
+++ duesanddos/duesanddos/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("login/", include("login.urls")),
     path("register/", include("users.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
