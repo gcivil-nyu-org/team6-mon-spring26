e85faaa3230aecc0e8ff003297ed2352ba2192b1
Forzana Rime <fkr206@nyu.edu>
2026-03-07 13:06:33 -0500

    Added password security checking

--- duesanddos/accounts/forms.py
+++ duesanddos/accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser
 
 class RegisterForm(forms.Form):
@@ -24,10 +25,27 @@ class RegisterForm(forms.Form):
 
     def clean(self):
         cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        first_name = cleaned_data.get("firstName")
+        last_name = cleaned_data.get("lastName")
+        email = cleaned_data.get("email")
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirmPassword")
 
         if password and confirm_password and password != confirm_password:
             self.add_error("confirmPassword", "Passwords do not match")
+
+        if password:
+            temp_user = CustomUser(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+            )
+
+            try:
+                validate_password(password, user=temp_user)
+            except ValidationError as e:
+                self.add_error("password", e)
 
         return cleaned_data
