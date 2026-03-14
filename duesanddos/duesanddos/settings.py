37df8a3bb6f6c53eb00f74f3a79ec4d178828dfa
Forzana Rime <fkr206@nyu.edu>
2026-03-14 16:19:01 -0400

    editing travis

--- duesanddos/duesanddos/settings.py
+++ duesanddos/duesanddos/settings.py
@@ -73,11 +73,11 @@ WSGI_APPLICATION = "duesanddos.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME", "duesanddos"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
