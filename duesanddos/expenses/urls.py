bc00cc3ff6e368844edccc5c12a2897c4729caf4
Anoop <avn2049@nyu.edu>
2026-04-02 20:24:47 -0400

    view: fixed the math logical errors, filtering

--- duesanddos/expenses/urls.py
+++ duesanddos/expenses/urls.py
@@ -2,15 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # --- Standard Expense Routes ---
     path("dashboard/add/", views.add_expense_pro, name="add_expense"), 
     path("expenses/", views.expenses_list_view, name="expenses_list"),
     path("expenses/add/", views.add_expense_pro, name="add_expense_pro"),
-    
-    # We comment this out because your team merged history into the main list view
-    # path("expense-history/", views.expense_history_view, name="expense_history"),
-    
-    # Updated to your team's new view name
     path("settle-split/<int:settlement_id>/", views.confirm_settlement, name="settle_split"),
     
     path(
@@ -19,7 +13,6 @@ urlpatterns = [
         name="delete_expense_pro",
     ),
 
-    # --- Your Settlement System Routes ---
     path("settle/request/", views.request_settlement, name="request_settlement"),
     path("settle/confirm/<int:settlement_id>/", views.confirm_settlement, name="confirm_settlement"),
     path(
