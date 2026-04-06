e1b08bcf0a8098a3e70770628723aeea5843766d
Astha Vairat <aav7237@nyu.edu>
2026-04-06 12:27:50 -0400

    Fix PEP8 formatting and URL configuration for Travis build

--- duesanddos/activities/tests/test_calendar.py
+++ duesanddos/activities/tests/test_calendar.py
@@ -2,15 +2,16 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from chores.models import Chore
-from households.models import Household, HouseholdMember
+from households.models import Household
 import datetime
+
 
 class CalendarApiTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='password')
-        self.client.login(username='testuser', password='password')
-        
+        self.user = User.objects.create_user(username="testuser", password="password")
+        self.client.login(username="testuser", password="password")
+
         # Setup household context
         self.household = Household.objects.create(name="Test Home")
         self.profile = self.user.profile
@@ -25,9 +26,9 @@ class CalendarApiTests(TestCase):
             created_by=self.user,
             repeat_type="ONE_TIME",
             has_due_date=True,
-            due_date=datetime.date.today()
+            due_date=datetime.date.today(),
         )
 
-        response = self.client.get(reverse('calendar_events_api'))
+        response = self.client.get(reverse("calendar_events_api"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Calendar Chore")
